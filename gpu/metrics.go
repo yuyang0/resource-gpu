516bd50b568955cfb8bd4a089ad16af3e71e51cf
YuYang <yyangplus@gmail.com>
2023-05-26 11:35:06 +0800

    update metrics and tests

--- gpu/metrics.go
+++ gpu/metrics.go
@@ -14,26 +14,14 @@ func (p Plugin) GetMetricsDescription(context.Context) (*plugintypes.GetMetricsD
 	resp := &plugintypes.GetMetricsDescriptionResponse{}
 	return resp, mapstructure.Decode([]map[string]interface{}{
 		{
-			"name":   "cpu_map",
-			"help":   "node available cpu.",
+			"name":   "gpu_capacity",
+			"help":   "node available gpu.",
 			"type":   "gauge",
 			"labels": []string{"podname", "nodename", "cpuid"},
 		},
 		{
-			"name":   "cpu_used",
-			"help":   "node used cpu.",
-			"type":   "gauge",
-			"labels": []string{"podname", "nodename"},
-		},
-		{
-			"name":   "memory_capacity",
-			"help":   "node available memory.",
-			"type":   "gauge",
-			"labels": []string{"podname", "nodename"},
-		},
-		{
-			"name":   "memory_used",
-			"help":   "node used memory.",
+			"name":   "gpu_used",
+			"help":   "node used gpu.",
 			"type":   "gauge",
 			"labels": []string{"podname", "nodename"},
 		},
@@ -51,24 +39,15 @@ func (p Plugin) GetMetrics(ctx context.Context, podname, nodename string) (*plug
 		{
 			"name":   "gpu_capacity",
 			"labels": []string{podname, nodename},
-			"value":  fmt.Sprintf("%+v", len(nodeResourceInfo.Capacity.GPUMap)),
-			"key":    fmt.Sprintf("core.node.%s.gpu", safeNodename),
+			"value":  fmt.Sprintf("%+v", nodeResourceInfo.CapLen()),
+			"key":    fmt.Sprintf("core.node.%s.gpu.capacity", safeNodename),
 		},
 		{
 			"name":   "gpu_used",
 			"labels": []string{podname, nodename},
-			"value":  fmt.Sprintf("%+v", len(nodeResourceInfo.Usage.GPUMap)),
+			"value":  fmt.Sprintf("%+v", nodeResourceInfo.UsageLen()),
 			"key":    fmt.Sprintf("core.node.%s.gpu.used", safeNodename),
 		},
-	}
-
-	for addr, info := range nodeResourceInfo.Usage.GPUMap {
-		metrics = append(metrics, map[string]interface{}{
-			"name":   "gpu_map",
-			"labels": []string{podname, nodename, addr},
-			"value":  fmt.Sprintf("%+v", 100),
-			"key":    fmt.Sprintf("core.node.%s.gpu.%s(%s)", safeNodename, addr, info.Product),
-		})
 	}
 
 	resp := &plugintypes.GetMetricsResponse{}
--- gpu/metrics_test.go
+++ gpu/metrics_test.go
@@ -13,7 +13,7 @@ func TestGetMetricsDescription(t *testing.T) {
 	md, err := cm.GetMetricsDescription(ctx)
 	assert.NoError(t, err)
 	assert.NotNil(t, md)
-	assert.Len(t, *md, 4)
+	assert.Len(t, *md, 2)
 }
 
 func TestGetMetrics(t *testing.T) {
@@ -23,6 +23,16 @@ func TestGetMetrics(t *testing.T) {
 	assert.Error(t, err)
 
 	nodes := generateNodes(ctx, t, cm, 1, -1)
-	_, err = cm.GetMetrics(ctx, "testpod", nodes[0])
+	resp, err := cm.GetMetrics(ctx, "testpod", nodes[0])
 	assert.NoError(t, err)
+	for _, mt := range *resp {
+		switch mt.Name {
+		case "gpu_capacity":
+			assert.Equal(t, mt.Value, "8")
+		case "gpu_used":
+			assert.Equal(t, mt.Value, "0")
+		default:
+			assert.True(t, false)
+		}
+	}
 }
