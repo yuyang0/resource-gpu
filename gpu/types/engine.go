1577d804efa808f246a0e68e80a7dd946a4f35df
YuYang <yyangplus@gmail.com>
2023-10-26 14:57:00 +0800

    add helper function to EngineParams

--- gpu/types/engine.go
+++ gpu/types/engine.go
@@ -8,3 +8,17 @@ type EngineParams struct {
 func (ep *EngineParams) Count() int {
 	return ep.ProdCountMap.TotalCount()
 }
+
+func (ep *EngineParams) DeepCopy() *EngineParams {
+	return &EngineParams{
+		ProdCountMap: ep.ProdCountMap.DeepCopy(),
+	}
+}
+
+func (ep *EngineParams) Sub(ep1 *EngineParams) {
+	ep.ProdCountMap.Sub(ep1.ProdCountMap)
+}
+
+func (ep *EngineParams) Add(ep1 *EngineParams) {
+	ep.ProdCountMap.Add(ep1.ProdCountMap)
+}
